33e46e0ed1f39c6ce84b1439d7559dccba162d65
Amrit Dhungana <amrit.dhungana@ibm.com@amrits-mbp.ibmmobiledemo.com>
2018-05-21 14:28:17 -0500

    Create array for pkg and test it

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-['httpd', 'httpd-devel', 'mariadb-server', 'php-mysql', 'php-pear',
-  'php-common', 'php-gd', 'php-devel', 'php', 'php-mbstring', 'php-cli',
-  'php-snmp', 'net-snmp-utils', 'net-snmp-libs', 'rrdtool'].each do |pkg|
-    package pkg
-  end
+
+apache_pkg = ['httpd', 'httpd-devel']
+db_pkg = ['mariadb-server']
+php_pkg = ['php-mysql', 'php-pear', 'php-common', 'php-gd', 'php-devel', 'php', 'php-mbstring', 'php-cli']
+snmp_pkg = ['php-snmp', 'net-snmp-utils', 'net-snmp-libs']
+rrd_pkg = ['rrdtool']
+
+pkg = (apache_pkg << db_pkg << php_pkg << snmp_pkg << rrd_pkg).flatten!
+
+pkg.each do |pkg|
+  package pkg
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -17,73 +17,16 @@ describe port(80), :skip do
   it { should_not be_listening }
 end
 
-package = ['httpd', 'httpd-devel', 'mariadb-server', 'php-mysql', 'php-pear',
-  'php-common', 'php-gd', 'php-devel', 'php', 'php-mbstring', 'php-cli',
-  'php-snmp', 'net-snmp-utils', 'net-snmp-libs', 'rrdtool']
+apache_pkg = ['httpd', 'httpd-devel']
+db_pkg = ['mariadb-server']
+php_pkg = ['php-mysql', 'php-pear', 'php-common', 'php-gd', 'php-devel', 'php', 'php-mbstring', 'php-cli']
+snmp_pkg = ['php-snmp', 'net-snmp-utils', 'net-snmp-libs']
+rrd_pkg = ['rrdtool']
 
-package.each do |pkg|
+pkg = (apache_pkg << db_pkg << php_pkg << snmp_pkg << rrd_pkg).flatten!
+
+pkg.each do |pkg|
   describe package(pkg) do
     it { should be_installed }
   end
 end
-
-
-# describe package('httpd') do
-#   it { should be_installed }
-# end
-#
-# describe package('httpd-devel') do
-#   it { should be_installed }
-# end
-#
-# describe package('mariadb-server') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-mysql') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-pear') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-common') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-gd') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-devel') do
-#   it { should be_installed }
-# end
-#
-# describe package('php') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-mbstring') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-cli') do
-#   it { should be_installed }
-# end
-#
-# describe package('php-snmp') do
-#   it { should be_installed }
-# end
-#
-# describe package('net-snmp-utils') do
-#   it { should be_installed }
-# end
-#
-# describe package('net-snmp-libs') do
-#   it { should be_installed }
-# end
-#
-# describe package('rrdtool') do
-#   it { should be_installed }
-# end
