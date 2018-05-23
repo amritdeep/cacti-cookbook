42589a4552b4996c2f43babc57d0dbe9dec8853f
Amrit Dhungana <amrit.dhungana@ibm.com@Amrits-MacBook-Pro.local>
2018-05-23 09:08:18 -0500

    Include firewalld dependency

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cacti'
 long_description 'Installs/Configures cacti'
+depends 'firewalld'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
